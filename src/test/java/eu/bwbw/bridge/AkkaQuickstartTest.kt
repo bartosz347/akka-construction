3ee1aaf0d8134d4d5ec7da9d3d63b93d4bd78fc5
Michał Sieczkowski <michal@ethworks.io>
2020-01-27 23:45:16 +0100

    Fix AkkaQuickstartTest

--- src/test/java/eu/bwbw/bridge/AkkaQuickstartTest.kt
+++ src/test/java/eu/bwbw/bridge/AkkaQuickstartTest.kt
@@ -3,20 +3,22 @@ package eu.bwbw.bridge
 import akka.actor.testkit.typed.javadsl.TestKitJunitResource
 import eu.bwbw.bridge.Greeter.Greet
 import eu.bwbw.bridge.Greeter.Greeted
-import org.junit.ClassRule
+import org.junit.Before
 import org.junit.Test
 
 class AkkaQuickstartTest {
+    private lateinit var testKit: TestKitJunitResource
+
+    @Before
+    fun setUp() {
+        testKit = TestKitJunitResource()
+    }
+
     @Test
     fun testGreeterActorSendingOfGreeting() {
         val testProbe = testKit.createTestProbe<Greeted>()
         val underTest = testKit.spawn(Greeter.create(), "greeter")
         underTest.tell(Greet("Charles", testProbe.ref))
         testProbe.expectMessage(Greeted("Charles", underTest))
-    }
-
-    companion object {
-        @ClassRule
-        val testKit = TestKitJunitResource()
     }
 }
