f7c5ae0ce7b9df64372640d099424658eae7f646
Bartosz Wojciechowski <bartosz347@gmail.com>
2020-01-28 21:19:56 +0100

    Draft GPS implementation

--- src/main/java/eu/bwbw/bridge/domain/Operation.kt
+++ src/main/java/eu/bwbw/bridge/domain/Operation.kt
@@ -3,6 +3,8 @@ package eu.bwbw.bridge.domain
 data class Operation(
         val name: String,
         val preconditions: Set<Goal>,
-        val adds: Set<Goal>,
-        val deletes: Set<Goal>
-)
+        val adds: MutableSet<Goal>,
+        val deletes: Set<Goal>,
+        var applied: Boolean = false
+) {
+}
