bce13fb7a5021fbcd08fb75f527f66213a67fc75
Bartosz Wojciechowski <bartosz347@gmail.com>
2020-01-29 13:35:03 +0100

    Fix set type

--- src/main/java/eu/bwbw/bridge/domain/Operation.kt
+++ src/main/java/eu/bwbw/bridge/domain/Operation.kt
@@ -3,7 +3,7 @@ package eu.bwbw.bridge.domain
 data class Operation(
         val name: String,
         val preconditions: Set<Goal>,
-        val adds: MutableSet<Goal>,
+        val adds: Set<Goal>,
         val deletes: Set<Goal>,
         var applied: Boolean = false
 ) {
