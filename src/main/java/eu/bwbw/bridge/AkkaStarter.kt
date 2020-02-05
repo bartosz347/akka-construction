acc72e06f63cc680bf3b1ecbfe571c9df250b0b1
Michał Sieczkowski <michal@ethworks.io>
2020-02-05 01:19:05 +0100

    Get path to config file from args, configure JAR packaging

--- src/main/java/eu/bwbw/bridge/AkkaStarter.kt
+++ src/main/java/eu/bwbw/bridge/AkkaStarter.kt
@@ -7,17 +7,40 @@ import eu.bwbw.bridge.domain.Config
 import eu.bwbw.bridge.domain.WorkerFunction
 import eu.bwbw.bridge.utils.send
 import java.io.File
+import kotlin.system.exitProcess
 
-fun main() {
-    val configFile = File("config/config.json")
-    val config = ConfigLoader().load(configFile)
+fun main(args: Array<String>) {
+    val config = loadConfig(args)
     val configWithFunctions = setWorkerFunctions(config) { Thread.sleep(1000) }
 
     val supervisor = ActorSystem.create(Supervisor.create(configWithFunctions), "supervisor")
     supervisor send Supervisor.Command.Begin
 }
 
-fun setWorkerFunctions(config: Config, doWork: WorkerFunction): Config {
+private fun loadConfig(args: Array<String>): Config {
+    if (args.size != 1) {
+        printUsage()
+        exitProcess(1)
+    }
+
+    return try {
+        val configFile = File(args[0])
+        ConfigLoader().load(configFile)
+    } catch (e: Exception) {
+        println(e.toString())
+        printUsage()
+        exitProcess(1)
+    }
+}
+
+private fun printUsage() {
+    println("""
+        Usage:
+        java -jar bridge-construction-1.0-all.jar <path-to-config-file>
+    """.trimIndent())
+}
+
+private fun setWorkerFunctions(config: Config, doWork: WorkerFunction): Config {
     val workersWithFunction = config.workers.map { it.copy(doWork = doWork) }.toSet()
     return config.copy(workers = workersWithFunction)
 }
